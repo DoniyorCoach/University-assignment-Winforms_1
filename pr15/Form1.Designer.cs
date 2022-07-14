39858e8c184fc6f78ee288fc29a839570b5241c3
Doniyor <doniyorcoach@gmail.com>
2022-07-15 03:21:32 +0500

    The first commit

---
+++ pr15/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace pr15 {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(208, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ToolTip";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseHover += new System.EventHandler(this.button1_mouseHover);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.BackColor = System.Drawing.Color.Green;
+            this.toolTip1.OwnerDraw = true;
+            this.toolTip1.Draw += new System.Windows.Forms.DrawToolTipEventHandler(this.toolTip1_Draw);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа №15, Аллаёров Дониёр";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ pr15/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace pr15 {
+	public partial class Form1 : Form {
+
+		private bool rightMouseClick, leftMouseClick;
+
+        public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_mouseHover(object sender, EventArgs e) {
+			toolTip1.SetToolTip(button1, "Моя формочка");
+		}
+
+		private void toolTip1_Draw(object sender, DrawToolTipEventArgs e) {
+			e.DrawBackground();
+			e.DrawBorder();
+			e.DrawText();
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e) {
+			double X = (double)e.X/Size.Width * 255;
+			double Y = (double)e.Y/Size.Height * 255;
+
+			int red = (int)X;
+			int green = (int)Y;
+			int blue = (int)Y;
+
+			BackColor = Color.FromArgb(red, green, blue);
+		}
+
+		private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+				CloseForm();
+		}
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+            if (e.KeyCode == Keys.F10)
+				CloseForm();
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this.Left -= 5;
+            }
+
+			if (e.KeyCode == Keys.Right)
+				this.Left += 5;
+			if (e.KeyCode == Keys.Up)
+				this.Top -= 5;
+			if (e.KeyCode == Keys.Down)
+				this.Top += 5;
+
+            Invalidate();
+		}
+
+		private void CloseForm()
+        {
+			DialogResult result = MessageBox.Show("Хотите выйти из программы?",
+				"Выход из программы", MessageBoxButtons.OKCancel);
+
+			if (result == DialogResult.OK)
+				this.Close();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+			if (e.Button == MouseButtons.Right)
+            {
+				rightMouseClick = true;
+
+				if (ModifierKeys == Keys.Shift)
+                {
+					this.Height += 5;
+					this.Width += 5;
+				}
+				else
+                {
+					this.Height -= 5;
+					this.Width -= 5;
+				}
+            }
+			if (e.Button == MouseButtons.Left)
+				leftMouseClick = true;
+
+			if (e.Button == MouseButtons.Middle || leftMouseClick == rightMouseClick)
+            {
+				this.Location = new Point(Cursor.Position.X - this.Width/2, Cursor.Position.Y - this.Height/2);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+			label2.Text = "Изменение размера: " + this.Height + " " + this.Width;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+			if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Right)
+            {
+				leftMouseClick = false;
+				rightMouseClick = false;
+            }
+        }
+    }
+}
